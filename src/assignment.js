7f64c87ce55d5026c1912c7842e4fb276980c664
Adesope Ayoade <>
2022-07-12 22:38:43 +0000

    check challenge-1 and challenge -2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,15 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+  
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  sum = 0
+for (let i=0; i< arrayOfNumbers.length;i++){
+  sum= arrayOfNumbers[i]+sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+return sum;
+}
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  totalEvenNumbers=0;
+  for(let index =0; index <arrayOfNumbers.length; index++){
+    if (arrayOfNumbers[index]%2==0){
+      totalEvenNumbers+=1
+    }
+  }
+    return totalEvenNumbers;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
